0b8bec67af8cfb1e1e95308771062eb6cb654e41
kyokoi_japan <kazutakayokoils@gmail.com>
2023-03-07 15:35:21 +0900

    finish lesson 6 and 7

--- src/hooks/transfer-nft.js
+++ src/hooks/transfer-nft.js
@@ -1,17 +1,44 @@
 import { useState } from 'react';
+import { VwblContainer } from '../container';
+import { useParams, useNavigate } from 'react-router-dom';
 
 export const useTransferNft = () => {
   const [isComplete, setIsComplete] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const { vwbl } = VwblContainer.useContainer();
+	const tokenId = Number(useParams().id); 
+	const navigate = useNavigate();
 
   // Lesson-7
-  const transferNft = (data) => {
-    console.log('submitted data', data);
-    setIsLoading(true);
-    setTimeout(() => {
-      setIsLoading(false);
-      setIsComplete(true);
-    }, 4000);
+  const transferNft = async(data) => {
+// vwblが存在しない場合
+if (!vwbl) {
+  console.log('Now your wallet is not connected. Please connect your wallet.');
+  return;
+}
+
+// フォームから受け取ったデータを抽出
+const { address } = data;
+
+//Loadingモーダルを表示する
+setIsLoading(true);
+
+try {
+  // VWBL NFTを送信
+  await vwbl.safeTransfer(address, tokenId);
+
+  //Loadingモーダルを閉じる
+  setIsLoading(false);
+
+  // 送信完了モーダルを表示する
+  setIsComplete(true);
+} catch (error) {
+  // エラー内容の表示
+  console.error(error);
+
+  //Loadingモーダルを閉じる
+  setIsLoading(false);
+}
   };
 
   // Lesson-7
